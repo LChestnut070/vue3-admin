3696103d1106f562968c9eda6d01ab8321e36bdd
LChestnut070 <lx2117764507@gmail.com>
2023-07-25 15:02:44 +0800

    提交未格式化的代码

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box">
+    <h2>23222</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+const str = "qwq"
+</script>
+
+<style scoped lang="scss">
+.box {
+  width: 200px;
+  height: 30px;
+  background-color: aquamarine;
+
+  h1 {
+    font-size: 20px;
+  }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+const qwq = function q() {
+  console.log(22)
+}
+qwq()
